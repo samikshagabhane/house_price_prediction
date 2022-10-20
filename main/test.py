12ef1203974d8be33459b34944e57bbbbeba679a
Samiksha Dadaji Gabhane <samiksha.gabhane@valuelabs.com>
2022-10-20 18:56:20 +0530

    Initial Upload

---
+++ main/test.py
@@ -0,0 +1,27 @@
+#import required libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pickle as pkl
+from sklearn.linear_model import LinearRegression
+import os
+
+
+car_data = pd.read_csv(r'./data/train_lite.csv')
+
+X_train, X_test, y_train, y_test = train_test_split(car_data.drop(['MedHouseVal'],axis=1), 
+                                                    car_data['MedHouseVal'], test_size=0.10, 
+                                                    random_state=101)
+
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+print("creating pkl file")
+print(os.getcwd())
+pkl.dump(model,open('./model.pkl','wb'))
+
+pred = model.predict(X_test)
+
+
+print(pred)
